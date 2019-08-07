3c1c18877441e6123247b41558c844973b320e05
Ricky <1107786871@qq.com>
2019-08-07 10:51:27 +0800

    a

--- src/main/java/com/example/config/SpringConfiguration.java
+++ src/main/java/com/example/config/SpringConfiguration.java
@@ -1,14 +1,23 @@
 package com.example.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.example.api.impl.TestApiImpl;
 
-public class SpringConfiguration {
+@Configuration
+public class SpringConfiguration extends WebMvcConfigurerAdapter {
 	@Bean
 	@Scope("prototype")
 	public TestApiImpl testApi() {
 		return new TestApiImpl();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 	}
 }
